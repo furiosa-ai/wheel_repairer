7d6b2e1ff3eacfcac27f0ee314c40a91c6739d59
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2024-08-20 21:17:30 +0900

    fix __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
 setup(
     name="wheel_repairer",
     version="0.1.0",
